d2481ecc8e0213ee5b0234336f3b5eb6c3013abc
dell <kirtidas90547@gmail.com>
2023-03-24 21:27:52 +0530

    my first git commit

---
+++ src/main/java/com/example/springbootexample/SpringBootExampleApplication.java
@@ -0,0 +1,34 @@
+package com.example.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/task")
+    public String task(@RequestParam String fname,@RequestParam String mname,@RequestParam String lname ) {
+        String goodName = "";
+        if (mname==""){
+            goodName = fname.concat(" ").concat(lname);
+        }else{
+           goodName = fname.concat(" ").concat(mname).concat(" ").concat(lname);
+        }
+
+        return String.format( goodName);
+    }
+
+}
